72bc9a8bc1abecddecf03995b2836ab25a88752d
berryjerrycherry <93579632+berryjerrycherry@users.noreply.github.com>
2021-11-04 20:03:35 +1100

    Add files via upload

--- Ruse Water Monitoring/script.js
+++ Ruse Water Monitoring/script.js
@@ -1,27 +1,35 @@
-let hour = document.getElementById("hour");
-let minute = document.getElementById("minute");
-let seconds = document.getElementById("seconds");
 
-let clock = setInterval(
-  function time() {
-    let date_now = new Date();
-    let hr = date_now.getHours();
-    let min = date_now.getMinutes();
-    let sec = date_now.getSeconds();
-    
+
+
+
+
+var hour = document.getElementById("hour");
+var minute = document.getElementById("minute");
+var seconds = document.getElementById("seconds");
+
+var clock = setInterval(
+  function time (){ 
+    var date_now = new Date();
+    var hr = date_now.getHours();
+    var min = date_now.getMinutes();
+    var sec = date_now.getSeconds();
     if(hr < 10){
-      hr = `0${hr}}`;
+      hr = "0" + hr;
+    
     }
     if(min < 10){
-      min = `0${min}`;
-    }
+      min = "0" + min;
     
+    }
     if(sec < 10){
-      sec = `0${sec}`;
+      sec = "0" + sec;
     }
 
     hour.innerHTML = hr;
     minute.innerHTML = min;
-    screen.innerHTML = sec;
-  }, 1000
+    seconds.innerHTML = sec;
+
+
+
+  },0
 )
